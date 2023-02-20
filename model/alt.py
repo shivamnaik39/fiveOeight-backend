f780a15e918f2131f5c05c9a61cb5bad04f3c601
grishmakadam <grishmakadam@yahoo.com>
2023-02-21 00:46:23 +0530

    ADD: ML model

---
+++ model/alt.py
@@ -0,0 +1,61 @@
+from matplotlib.pyplot import imread
+
+from matplotlib.pyplot import imshow
+
+from tensorflow.keras.preprocessing import image
+
+from tensorflow.keras.applications.imagenet_utils import decode_predictions
+
+from tensorflow.keras.applications.imagenet_utils import preprocess_input
+import cv2``
+
+
+
+ 
+
+img_path = ''
+
+
+ 
+
+#img = image.load_img(img_path, target_size=(224, 224))
+
+#x = img.img_to_array(img)
+
+
+ 
+
+img = cv2.imread(img_path)
+
+img = cv2.resize(img, (224, 224))
+
+
+ 
+
+x = np.expand_dims(img, axis=0)
+
+x = preprocess_input(x)
+
+
+ 
+
+print('Input image shape:', x.shape)
+
+
+ 
+
+my_image = imread(img_path)
+
+imshow(my_image)
+
+from tensorflow.keras.models import load_model
+
+ 
+
+# load model
+
+loaded_model = load_model('model.h5')
+
+preds=loaded_model.predict(x)
+
+preds
