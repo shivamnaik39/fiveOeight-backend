b1794570bc0b153f521a32bf9017e9d2ee77d716
grishma-kadam <grishmakadam@yahoo.com>
2023-02-23 20:35:58 +0530

    model file updated

--- model/alt.py
+++ model/alt.py
@@ -1,61 +1,39 @@
 from matplotlib.pyplot import imread
-
-from matplotlib.pyplot import imshow
-
 from tensorflow.keras.preprocessing import image
-
 from tensorflow.keras.applications.imagenet_utils import decode_predictions
-
 from tensorflow.keras.applications.imagenet_utils import preprocess_input
 import cv2
-
-
-
- 
-
-img_path = ''
-
-
- 
-
-#img = image.load_img(img_path, target_size=(224, 224))
-
-#x = img.img_to_array(img)
-
-
- 
-
-img = cv2.imread(img_path)
-
-img = cv2.resize(img, (224, 224))
-
-
- 
-
-x = np.expand_dims(img, axis=0)
-
-x = preprocess_input(x)
-
-
- 
-
-print('Input image shape:', x.shape)
-
-
- 
-
-my_image = imread(img_path)
-
-imshow(my_image)
-
 from tensorflow.keras.models import load_model
+import numpy as np
+import urllib.request
 
- 
 
-# load model
+def get_alt_text(img_path):
 
-loaded_model = load_model('model.h5')
+    #img = image.load_img(img_path, target_size=(224, 224))
 
-preds=loaded_model.predict(x)
+    #x = img.img_to_array(img)
+    urllib.request.urlretrieve(img_path, 'model\image.jpg')
+    img = cv2.imread('model\image.jpg')
 
-preds
+    img = cv2.resize(img, (224, 224))
+
+    x = np.expand_dims(img, axis=0)
+
+    x = preprocess_input(x)
+
+
+
+    # load model
+
+    loaded_model = load_model('model\model.h5')
+
+    preds=loaded_model.predict(x)
+    if preds[0][0]>preds[0][1] and preds[0][0]>preds[0][2]:
+        return "Laptop"
+    elif preds[0][1]>preds[0][2]:
+        return "Smartphone"
+    else:
+        return "Television"
+
+
--- model/get_alt.py
+++ model/get_alt.py
@@ -1,6 +1,6 @@
 import urllib
 import os
-
+import alt
 
 def get_alt(src):
     # Use urllib.parse.urlsplit to split the src into components
@@ -9,5 +9,6 @@ def get_alt(src):
 
     # Use os.path.basename to extract the file name from the path
     filename = os.path.basename(path)
-
+    print(alt.get_alt_text("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTanH4tQ8qNti33Xnz1OoyKJC_687ROIutEx4CfeRa9og&usqp=CAU&ec=48600112"))
     return filename
+
